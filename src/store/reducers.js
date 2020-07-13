5059d678da8d965b0b7232052380c2a13962ce7a
Crowdbotics <team@crowdbotics.com>
2020-07-13 20:05:11 +0000

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,6 +4,9 @@ const initialState = {}
 
 export default function apiReducer(state = initialState, action) {
   switch (action.type) {
+    case types.GET_PETS_LIST:
+    case types.GET_PETS_LIST_SUCCEEDED:
+    case types.GET_PETS_LIST_FAILED:
     default:
       return state
   }
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,8 +1,19 @@
 import { put, call, all, spawn } from "redux-saga/effects"
 import { apiService } from "./services"
 import * as types from "./constants"
+function* get_pets_listWorker(action) {
+  try {
+    const result = yield call(apiService.get_pets_list, action)
+    yield put(actions.get_pets_listSucceeded(result))
+  } catch (err) {
+    yield put(actions.get_pets_listFailed(err))
+  }
+}
+function* get_pets_listWatcher() {
+  yield takeEvery(types.GET_PETS_LIST, get_pets_listWorker)
+}
 export default function* rootSaga() {
-  const sagas = []
+  const sagas = [get_pets_listWatcher]
   yield all(
     sagas.map(saga =>
       spawn(function*() {
